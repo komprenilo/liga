5a1020f433c49087b2f261745aa53aa458e1bdbc
Darcy Shen <da@tubi.tv>
2023-01-09 22:49:52 +0800

    liga-mlflow and liga-sklearn

--- python/tests/liga/sklearn/mlflow_test.py
+++ python/tests/liga/sklearn/mlflow_test.py
@@ -1,23 +1,19 @@
 from liga.sklearn.mlflow import _get_model_type
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 
 def test_get_model_type():
-    from sklearn.linear_model import LinearRegression
-
     regressor = LinearRegression()
     assert _get_model_type(regressor) == "liga.sklearn.models.regressor"
-
-    from sklearn.linear_model import RidgeClassifier
 
     classifier = RidgeClassifier()
     assert _get_model_type(classifier) == "liga.sklearn.models.classifier"
 
-    from sklearn.decomposition import PCA
-
     pca = PCA(n_components=2)
     assert _get_model_type(pca) == "liga.sklearn.models.transformer"
-
-    from sklearn.cluster import KMeans
 
     kmeans = KMeans(n_clusters=2, random_state=0)
     assert _get_model_type(kmeans) == "liga.sklearn.models.cluster"
