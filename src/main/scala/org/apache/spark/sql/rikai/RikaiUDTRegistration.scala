e922f2f6ca1af1c361b8f60604f0d7d8d2e7108d
Da Shen <da@tubi.tv>
2023-02-08 14:19:50 +0800

    wip

--- src/main/scala/org/apache/spark/sql/rikai/RikaiUDTRegistration.scala
+++ src/main/scala/org/apache/spark/sql/rikai/RikaiUDTRegistration.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Liga authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.spark.sql.rikai
 
 import org.apache.spark.internal.Logging
@@ -11,22 +26,15 @@ object RikaiUDTRegistration extends Serializable with Logging {
   def register(schemaName: String, dataType: DataType): Unit = {
     if (schemaMap.contains(schemaName)) {
       logWarning(
-        s"Cannot register UDT for ${schemaName}, which is already registered."
+        s"Cannot register UDT for `${schemaName}`, which is already registered."
       )
     } else {
-      // When register UDT with class name, we can't check if the UDT class is an UserDefinedType,
-      // or not. The check is deferred.
       schemaMap += ((schemaName, dataType))
     }
   }
 
   def exists(schemaName: String): Boolean = schemaMap.contains(schemaName)
 
-  /** Returns the Class of UserDefinedType for the name of a given user class.
-    *
-    * @param schemaName schema name of user class
-    * @return Option value of the Class object of UserDefinedType
-    */
   def getUDT(schemaName: String): Option[DataType] = {
     schemaMap.get(schemaName)
   }
