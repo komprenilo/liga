1f247a0c2880421c64ab9a0eb8b5d0481ae62679
Da Shen <da@tubi.tv>
2023-08-25 16:58:16 +0800

    fix

--- liga/test/src/net/xmacs/liga/model/RegistryTest.scala
+++ liga/test/src/net/xmacs/liga/model/RegistryTest.scala
@@ -23,7 +23,7 @@ import org.scalatest.funsuite.AnyFunSuite
 class RegistryTest extends AnyFunSuite with BeforeAndAfter {
 
   before {
-    Register.reset
+    Registry.reset
     Registry.registerAll(
       Map(
         Registry.REGISTRY_IMPL_PREFIX + "test.impl" -> "net.xmacs.rikai.sql.model.testing.TestRegistry",
